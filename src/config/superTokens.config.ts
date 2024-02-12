d71d036c04822f0619dce0c6f5f54986d85f7ce7
Scott <scott@scottbenton.dev>
2024-02-12 18:09:00 -0500

    fix(build): Hoping to get more information about the build variables

--- src/config/superTokens.config.ts
+++ src/config/superTokens.config.ts
@@ -5,19 +5,21 @@ import { SuperTokensConfig } from "supertokens-auth-react/lib/build/types";
 
 export function getApiDomain() {
   const apiPort = process.env.LOCAL_AUTH_API_PORT || 3001;
-  const apiUrl = process.env.AUTH_API_URL || `http://localhost:${apiPort}`;
-  return apiUrl;
+  const environmentVariableURL = process.env.AUTH_API_URL;
+
+  if (environmentVariableURL) {
+    console.debug(environmentVariableURL);
+    return environmentVariableURL;
+  } else {
+    console.debug(apiPort);
+    return `http://localhost:${apiPort}`;
+  }
 }
 
 export function getWebsiteDomain() {
   const websitePort = location.port || 3000;
   const websiteUrl = location.origin || `http://localhost:${websitePort}`;
   return websiteUrl;
-}
-
-export function getSessionTokenFrontendDomain() {
-  const domain = process.env.AUTH_SESSION_TOKEN_DOMAIN as string | undefined;
-  return domain;
 }
 
 export const superTokensConfig: SuperTokensConfig = {

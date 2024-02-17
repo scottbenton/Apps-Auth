e46d9b481bb1e173eff953e7f6459e31ed929fdf
Scott Benton <scott@scottbenton.dev>
2024-02-17 15:42:52 -0500

    fix(build): Updated webpack

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,19 +1,16 @@
 import { constructModuleWebpackConfig } from "@scottbenton/apps-build";
 import { ModuleScope } from "@scottbenton/apps-config";
 
-const config = constructModuleWebpackConfig(
-  {
-    name: ModuleScope.Authentication,
-    dependencies: {},
-    exposes: {
-      "./AuthProvider": "./src/AuthProvider.tsx",
-      "./useCurrentUser": "./src/context/useCurrentUser",
-      "./roles": "./src/types/Roles",
-      "./authRoutes": "./src/authRoutes.ts",
-      "./SessionBlocker": "./src/components/SessionBlocker.tsx",
-    },
+const config = constructModuleWebpackConfig({
+  name: ModuleScope.Authentication,
+  dependencies: {},
+  exposes: {
+    "./AuthProvider": "./src/AuthProvider.tsx",
+    "./useCurrentUser": "./src/context/useCurrentUser",
+    "./roles": "./src/types/Roles",
+    "./authRoutes": "./src/authRoutes.ts",
+    "./SessionBlocker": "./src/components/SessionBlocker.tsx",
   },
-  true
-);
+});
 
 export default config;
